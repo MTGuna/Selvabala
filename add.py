4b5a10dd81a5bfd20d6007e1dbc8c0136c0b24e7
Selvabala <140789930+TSelvabala@users.noreply.github.com>
2025-03-11 12:02:20 +0530

    Delete add.py

--- add.py
+++
@@ -1,8 +0,0 @@
-import sys
-print('Number of arguments:',len(sys.argv),'arguments.')
-print('Argument List:',str(sys.argv))
-x=int(sys.argv[0])
-y=int(sys.argv[2])
-z=x+y
-print("x=",x,"y=",y,"z=",z)
-print(z)
